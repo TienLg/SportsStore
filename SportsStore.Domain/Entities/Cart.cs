2561a18a2bfa4f39c5d4c3c357b4631d65724dbf
TienLg <tiendluong@gmail.com>
2015-09-03 18:20:14 +0700

    Complete Cart Processing

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product prod, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(l => l.Product.ProductID == prod.ProductID)
+                .FirstOrDefault();
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine() { Product = prod, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product prod){
+            lineCollection.RemoveAll(l => l.Product.ProductID == prod.ProductID);
+        }
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(l => l.Product.Price * l.Quantity);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
